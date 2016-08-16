3e86aae17e931235b7d26332c986eada9ff2fbd5
Gabriel Gianordoli <gianordoligabriel@gmail.com>
2016-08-16 19:45:14 -0400

    updated port setting

--- app.js
+++ app.js
@@ -73,7 +73,7 @@ var cities = {
 
 
 /*---------- BASIC SETUP ----------*/
-var PORT = 8080;
+var port = process.env.PORT || 3000;
 app.listen(PORT, function(){
 	console.log('Express server is running at ' + PORT);
 });
